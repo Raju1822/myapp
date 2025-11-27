bfdf2952a16ae903d9241d24d38a9619faad67aa
Sahu, Raju <raju_223+uhg@optum.com>
2025-11-27 20:30:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import MemberDashboard from './pages/MemberDashboard';
+import ManagerDashboard from './pages/ManagerDashboard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/manager-dashboard" element={<ManagerDashboard />} />
+        <Route path="/member-dashboard" element={<MemberDashboard />} />
+      </Routes>
+    </BrowserRouter>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/pages/MemberDashboard.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const MemberDashboard = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Member Dashboard</h1>
+      <p>Welcome! Here you will see tasks and productivity metrics.</p>
+    </div>
+  );
+};
+
+export default MemberDashboard;
